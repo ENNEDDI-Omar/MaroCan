d7f50360e6173637e0e7568383f87d57bb3a526f
OmarENNEDDI <enneddiomar@gmail.com>
2024-04-04 21:33:32 +0000

    modiff de l email input en register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -48,8 +48,8 @@
                     <label for="email" class="text-left text-white">{{ __('Email:') }}</label>
                     <input id="email"
                         class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
-                        type="email" name="email" value="{{ old('email') }}" required autocomplete="username"
-                        placeholder="example@gmail.com" />
+                        type="email" name="email" value="{{ old('email') }}" autocomplete="email"
+                        placeholder="example@gmail.com" required />
                     @error('email')
                         <p class="mt-2 text-red-500">{{ $message }}</p>
                     @enderror
@@ -64,17 +64,17 @@
                         <p class="mt-2 text-red-500">{{ $message }}</p>
                     @enderror
                 </div>
-                <div class="pb-2 pt-4">
+                {{-- <div class="pb-2 pt-4">
                     <label for="password_confirmation"
                         class="text-left text-white">{{ __('Confirm Password:') }}</label>
                     <input id="password_confirmation"
                         class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
-                        type="password" name="password_confirmation" required autocomplete="new-password"
+                        type="password" name="password_confirmation" required 
                         placeholder="********" />
                     @error('password_confirmation')
                         <p class="mt-2 text-red-500">{{ $message }}</p>
                     @enderror
-                </div>
+                </div> --}}
 
                 <div class="pb-2 pt-4">
                     <label for="phone" class="text-left text-white">{{ __('Phone:') }}</label>
@@ -86,6 +86,17 @@
                         <p class="mt-2 text-red-500">{{ $message }}</p>
                     @enderror
                 </div>
+                {{-- <div>
+                    <label for="dropzone-file" class="flex items-center px-3 py-3 mx-auto mt-6 text-center bg-white border-2 border-dashed rounded-lg cursor-pointer dark:border-gray-600 dark:bg-gray-900">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-gray-300 dark:text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12" />
+                        </svg>
+ 
+                        <h2 class="mx-3 text-gray-400">Profile Photo</h2>
+ 
+                        <input id="dropzone-file" type="file" name="Profil" class="hidden" />
+                    </label>
+                </div> --}}
                 <div class="pb-2 pt-4">
                    <div>
                        <a href="{{ route('login') }}"
