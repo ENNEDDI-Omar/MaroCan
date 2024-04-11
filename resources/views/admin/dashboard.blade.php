0db9fab5183a52131a4b6ffb4e60e4390922d1b3
OmarENNEDDI <enneddiomar@gmail.com>
2024-04-11 00:53:58 +0000

    Ajout de l'admin dashboard view

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.auth')
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Welcome to the admin dashboard</h1>
+                
+            </div>
+        </div>
+    </div>
+@endsection
