2ae9e29766920ccf67ce37fe08a9a4c0c01bc349
OmarENNEDDI <enneddiomar@gmail.com>
2024-04-25 01:57:29 +0100

    Add ReservationStoreRequest and ReservationUpdateRequest for reservation validation

---
+++ app/Http/Requests/ReservationStoreRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ReservationStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'user_id' => 'required|exists:users,id',
+            'football_match_id' => 'required|exists:football_matches,id',
+            'zone' => 'required|in:gradins,tribunes,vip',
+            'number_of_tickets' => 'required|integer|min:1'
+            
+        ];
+    }
+}
