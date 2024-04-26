b9dbb598648d6a2dae76522f5e01d4359cdc0a9c
OmarENNEDDI <enneddiomar@gmail.com>
2024-04-26 11:06:44 +0100

    Add Manager model and update MediaPlatform model

--- app/Models/Manager.php
+++ app/Models/Manager.php
@@ -4,28 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 
-class MediaPlatform extends Model implements HasMedia
+class Manager extends Model implements HasMedia
 {
     use HasFactory, SoftDeletes, InteractsWithMedia;
 
     protected $fillable = [
         'name',
-        'media_platform_code',
-        'type',
+        'team_id',
+
     ];
 
-    public function accreditationBadge(): HasMany
+    public function team(): BelongsTo
     {
-        return $this->hasMany(AccreditationBadge::class);
+        return $this->belongsTo(Team::class, 'team_id');
     }
-    
-    
-
-    
 }
