cf19153e892fe15ea1dfa97ea47e5879ab405265
OmarENNEDDI <enneddiomar@gmail.com>
2024-04-29 07:52:12 +0100

    Update routes/web.php with new controllers and routes

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,27 @@
 
 use App\Http\Controllers\Admin\AccreditationBadgeController;
 use App\Http\Controllers\Admin\AdminDashboardController;
+use App\Http\Controllers\Admin\ApplicationController as AdminApplicationController;
 use App\Http\Controllers\Admin\ArticleController;
 use App\Http\Controllers\Admin\MediaPlatformController;
 use App\Http\Controllers\Admin\TagController;
 use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\Admin\VolunteeringOfferController;
 use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\Auth\ForgotPasswordController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Journalist\ArticleController as JournalistArticleController;
+use App\Http\Controllers\User\AccreditationController;
+use App\Http\Controllers\User\ApplicationController;
+use App\Http\Controllers\User\ArticleController as UserArticleController;
+use App\Http\Controllers\User\MatchController;
+use App\Http\Controllers\User\ReservationController;
+use App\Http\Controllers\User\VolunteerOfferController;
+use App\Models\AccreditationBadge;
+use App\Models\Application;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -56,7 +67,7 @@ Route::middleware("auth")->group(function(){
 
 
 // // Route pour la page d'accueil
-Route::resource('home', HomeController::class); 
+ 
  
 // // Route pour le dashboard de l'admin
 
@@ -69,31 +80,77 @@ Route::resource('home', HomeController::class);
 
 ///////////////ADMIN////////////////////
 
-Route::prefix('admin')->name('admin.')->middleware('auth')->group(function () {
+Route::prefix('admin')->name('admin.')->middleware('admin')->group(function () {
     //// Route pour les crud des utilisateurs
     Route::resource('users', UserController::class);
 
     // // Route pour banner l'utilisateur
     // Route::patch('users/{user}/ban', [UserController::class, 'ban']);
 
-    // // Route pour le dashboard de l'admin
-    Route::get('dashboard', [AdminDashboardController::class, 'index'])->name('dashboard');
-
-   // // Route pour les Articles
-   Route::resource('articles', ArticleController::class);
-
-   // //  Route pour les Tags
-   Route::resource('tags', TagController::class);
-
-  // //Route pour les Accreditations
+ // // Route pour le dashboard de l'admin
+  Route::get('dashboard', [AdminDashboardController::class, 'index'])->name('dashboard');
+ // // Route pour les Articles
+  Route::resource('articles', ArticleController::class);
+ // //  Route pour les Tags
+  Route::resource('tags', TagController::class);
+// //Route pour les Accreditations
   Route::resource('accreditations', AccreditationBadgeController::class);
-
-  // // Route pour les MediaPlatforms
+ // // Route pour les MediaPlatforms
   Route::resource('media-platforms', MediaPlatformController::class);
+// Route pour la gestion des VolunteeringOffers par l'admin
+  Route::resource('volunteering-offers', VolunteeringOfferController::class);
+// Route pour la gestion des Applications par l'admin
+    Route::resource('applications', AdminApplicationController::class)->only(['index', 'show', 'update']);
 
 
 });
 
+// Route pour l'utilisateur
+
+Route::prefix('user')->name('user.')->middleware('user')->group(function () {
+ //route vers le home 
+ Route::resource('home', HomeController::class);
+ //route vers la page des offres de bénévolat
+ Route::resource('matchs', MatchController::class)->only(['index', 'show']);
+    Route::get('matchs/search', [MatchController::class, 'search'])->name('matchs.search');
+    Route::resource('volunteering-offers', VolunteerOfferController::class);
+ // route pour le search des offres de bénévolat
+     Route::get('volunteering-offers/search', [VolunteerOfferController::class, 'search'])->name('volunteering-offers.search');   
+ //route vers la page des articles
+    Route::resource('articles', UserArticleController::class)->only(['index', 'show']);
+ // route pour search des articles
+     Route::get('articles/search', [UserArticleController::class, 'search']);
+ // route pour le formilaire du badge d'accreditation
+    Route::resource('accreditations', AccreditationController::class)->only(['create', 'store']);
+   
+ // route pour l'applicattion de l'utilisateur aux offres de bénévolat
+    Route::resource('applications', ApplicationController::class)->only(['store']);
+ // route vers la visualisation des match
+    Route::resource('matchs', MatchController::class)->only(['index', 'show']);
+ 
+  
+ // route pour la reservation des tickets des matchs
+     Route::get('reservations/create/{match}', [ReservationController::class, 'create'])->name('reservations.create');
+     Route::resource('reservations', ReservationController::class)->except(['create']);
+
+   // In routes/web.php
+   //Route::get('reservations/create/{id}', [ReservationController::class, 'create'])->name('user.reservation.create');
+
+
+
+
+
+
+
+});
+
+
+Route::prefix('journalist')->name('journalist.')->middleware('journalist')->group(function () {
+
+// // Route vers ces Articles 
+     Route::resource('articles', JournalistArticleController::class);
+
+});
 
 
 
